8f9565d714c137559a885782f7d1abd77e267b0f
Mohammed A.Baqi <alaghbri.mohammed@gmail.com>
2023-10-16 04:37:35 +0300

    First Commit

---
+++ Data/SchoolContext.cs
@@ -0,0 +1,30 @@
+﻿#define First // LAST First
+#if First
+#region snippet_first
+using DockerUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DockerUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext (DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
+#endregion
+#elif LAST
+#endif
